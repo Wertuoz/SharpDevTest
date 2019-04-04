9ac87bae1cce3b170bc31e56375ac8966d3ad2db
Anton Trofimenko <troantuan@gmail.com>
2019-04-04 13:51:54 +0300

    Initial Commit

---
+++ SharpDevTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharpDevTest
+//
+//  Created by Anton Trofimenko on 04/04/2019.
+//  Copyright © 2019 Anton Trofimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
